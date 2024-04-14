0a48fe4f6c1589f7c49a52391ea4bac4b9e436f2
Rishabh Naik <70832462+RishabhNaik@users.noreply.github.com>
2024-04-14 20:03:18 +0530

    setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/product/<product_name>')
+def show_product(product_name):
+    print("Product Name:", product_name)
+    return render_template('index.html', product_name=product_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
